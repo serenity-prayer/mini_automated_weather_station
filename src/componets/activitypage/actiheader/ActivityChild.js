6531dbc429d6e4ab8dc0bd06be85814cdfbdc3b0
serenity-prayer <bsc-99-16@cc.ac.mw>
2022-03-10 17:25:20 +0300

    cleaned 2.0

--- src/componets/activitypage/actiheader/ActivityChild.js
+++ src/componets/activitypage/actiheader/ActivityChild.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './Activity.css';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
+
 function ActivityChild(){
+    const [district, setDistrict, crops, setCrops] = React.useState('');
+    const handleChange = (event) => {
+        setDistrict(event.target.value);
+        
+      };
+   
     return(
        <div>
             <div className="row">
@@ -41,26 +54,114 @@ function ActivityChild(){
                 </div>
             </div>
 
-            <div className="row">
+            <div className="row1">
                 <h4 className='m-1 row-header text-center text-primary card-titled'>input your data</h4>
                 <div className="">
                   <div className='card-group '>
 
-                 <h1>
-                    <TextField id="outlined-basic" label="enter crop" variant="outlined" />
-                    <h1>  </h1>
-                    <TextField id="outlined-basic" label="enter district" variant="outlined" />
-                    <h1>  </h1>
-                    <Button variant="contained" disableElevation>  submit  </Button>
-    
-   
+                 <h1><h1>
+                 <Box sx={{ minWidth: 150 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">District</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={district}
+                        label="district"
+                        onChange={handleChange}
+                        >
+                        <MenuItem value={10}>Chitipa</MenuItem>
+                        <MenuItem value={20}>Chitipa</MenuItem>
+                        <MenuItem value={30}>Karonga</MenuItem>
+                        <MenuItem value={40}>Likoma</MenuItem>
+                        <MenuItem value={50}>Mzimba</MenuItem>
+                        <MenuItem value={60}>NkhataBay</MenuItem>
+                        <MenuItem value={70}>Rumphi</MenuItem>
+                        <MenuItem value={80}>Dedza</MenuItem>
+                        <MenuItem value={90}>Dowa</MenuItem>
+                        <MenuItem value={100}>Kasungu</MenuItem>
+                        <MenuItem value={110}>Lilongwe</MenuItem>
+                        <MenuItem value={120}>Mchinji</MenuItem>
+                        <MenuItem value={130}>Nkhotakota</MenuItem>
+                        <MenuItem value={140}>Ntcheu</MenuItem>
+                        <MenuItem value={150}>Ntchisi</MenuItem>
+                        <MenuItem value={160}>Salima</MenuItem>
+                        <MenuItem value={170}>Balaka</MenuItem>
+                        <MenuItem value={180}>Blantyre</MenuItem>
+                        <MenuItem value={190}>Chikwawa</MenuItem>
+                        <MenuItem value={200}>Chiradzulu</MenuItem>
+                        <MenuItem value={210}>Machinga</MenuItem>
+                        <MenuItem value={220}>Mangochi</MenuItem>
+                        <MenuItem value={230}>Mulanje</MenuItem>
+                        <MenuItem value={240}>Mwanza</MenuItem>
+                        <MenuItem value={250}>Neno</MenuItem>
+                        <MenuItem value={260}>Nsanje</MenuItem>
+                        <MenuItem value={270}>Phalombe</MenuItem>
+                        <MenuItem value={280}>Thyolo</MenuItem>
+                        <MenuItem value={290}>Zomba</MenuItem>
+                        </Select>
+                    </FormControl>
+                    </Box>
+
                  </h1>
 
-                  </div>
-                  </div>
-                  </div>
+                 <Box sx={{ minWidth: 150 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">crops</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={crops}
+                        label="Crops"
+                        onChange={handleChange}
+                        >
+                        <MenuItem value={10}>Maize</MenuItem>
+                        <MenuItem value={20}>Rice</MenuItem>
+                        <MenuItem value={30}>Groundnuts</MenuItem>
+                        <MenuItem value={30}>Sugar Cane</MenuItem>
+                        <MenuItem value={40}>Legumes</MenuItem>
+                        <MenuItem value={50}>banana</MenuItem>
+                        <MenuItem value={60}>Tea</MenuItem>
+                        <MenuItem value={70}>Soya</MenuItem>
+                        <MenuItem value={80}>Tobacco</MenuItem>
+                        <MenuItem value={90}>Cotton</MenuItem>
+                        <MenuItem value={80}>Cassava</MenuItem>
+                        <MenuItem value={90}>Sweet potatoes</MenuItem>
+                        <MenuItem value={90}>Irish potatoes</MenuItem>
+                        
+                        
+                     
+                        </Select>
+                    </FormControl>
+                    </Box>
+                        <h1>  
+                         <h1> 
+                        </h1>
+                        </h1>
+                
+                    <h1>  
+                         <h1>   
+                        </h1>
+                        </h1>
+                    <Button variant="contained" disableElevation>  submit
+                     </Button>
+
+               
+                </h1>
+                <div>
+                <h1> 
+                <h1>
+      
+
+                </h1>
+                </h1>
+                </div>
+              
+                </div>
+                </div>
+                </div>
             
-            <div className='row'>
+            <div className='row1'>
 
          
             </div>
